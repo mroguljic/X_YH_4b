ecc1ba263f67099141a3f36b94dd62b8ad294883
matej <matej.roguljic@gmail.com>
2020-03-21 11:49:07 +0100

    Wrapped main in one function

--- deltaRMatching.py
+++ deltaRMatching.py
@@ -122,92 +122,105 @@ def plotHistogram(data,outfile,title,xlabel,ylabel="N"):
     plt.clf()
 
 
-tfile = r.TFile.Open("E2FC3D3A-4D94-494D-BD56-524A28EF3C3F.root")
-
-print("Number of entries: {0}".format(tfile.Events.GetEntriesFast()))
-matchedHiggs        = 0
-matchedY            = 0
-matchedBoth         = 0
-matchedHiggsMasses  = []
-matchedHiggsPts     = []
-matchedYMasses      = []
-matchedYPts         = []
-
-for i,event in enumerate(tfile.Events):
-    # if(i>1000):
-    #     break
-
-    if(i%100000==0):
-        print(i)
-
-    higgsPhiEta         = getHigssPhiEta(event)
-    YPhiEta             = getYPhiEta(event)
-    b1PhiEta,b2PhiEta   = getBFromHiggsPhiEta(event)
-    b3PhiEta,b4PhiEta   = getBFromYPhiEta(event)
-    matchedHiggsJetIdxs = []
-    matchedYJetIdxs     = []
-    hDeltaRs            = []
-    YDeltaRs            = []
-    matchedHiggsFlag    = False
-    matchedYFlag        = False
-
-    for fatJetIdx in range(event.nFatJet):
-        fatJetPhiEta = event.FatJet_phi[fatJetIdx],event.FatJet_eta[fatJetIdx]
-
-        h_JetDR  = deltaR(fatJetPhiEta,higgsPhiEta)
-        b1_JetDR = deltaR(fatJetPhiEta,b1PhiEta)
-        b2_JetDR = deltaR(fatJetPhiEta,b2PhiEta)
-
-        Y_JetDR  = deltaR(fatJetPhiEta,YPhiEta)
-        b3_JetDR = deltaR(fatJetPhiEta,b3PhiEta)
-        b4_JetDR = deltaR(fatJetPhiEta,b4PhiEta)        
-
-        if (h_JetDR<0.8 and b1_JetDR<0.8 and b2_JetDR<0.8):
-            matchedHiggsJetIdxs.append(fatJetIdx)
-            hDeltaRs.append(h_JetDR)
-            matchedHiggsFlag=True
-            matchedHiggsMasses.append(event.FatJet_mass[fatJetIdx])
-            matchedHiggsPts.append(event.FatJet_pt[fatJetIdx])
-
-        if (Y_JetDR<0.8 and b3_JetDR<0.8 and b4_JetDR<0.8):
-            matchedYJetIdxs.append(fatJetIdx)
-            YDeltaRs.append(Y_JetDR)
-            matchedYFlag=True
-            matchedYMasses.append(event.FatJet_mass[fatJetIdx])
-            matchedYPts.append(event.FatJet_pt[fatJetIdx])
+def plotHistogram(data,outfile,title,xlabel,xlow,xup):
+    plt.hist(data,100)
+    plt.xlabel(xlabel)
+    plt.title(title)
+    plt.grid(True)
+    plt.savefig(outfile)
+    plt.clf()
 
 
-    if(len(matchedHiggsJetIdxs)>1):
-        print("Matched more than one AK8 to Higgs")
-        print("Higgs phi {0}, eta {1}".format(*higgsPhiEta))
-        for j,idx in enumerate(matchedHiggsJetIdxs):
-            print("Jet{0} - pt {1}, mass {2}, phi {3}, eta {4}".format(j,event.FatJet_mass[idx],event.FatJet_pt[idx],event.FatJet_phi[idx],event.FatJet_eta[idx],))
-        print("DeltaRs (AK8-Higgs) {0},{1}".format(*hDeltaRs))
-        print('\n')
+def analyzeFile(infile,outPrefix):
+    tfile = r.TFile.Open(infile)
+
+    print("Number of entries: {0}".format(tfile.Events.GetEntriesFast()))
+    matchedHiggs        = 0
+    matchedY            = 0
+    matchedBoth         = 0
+    matchedHiggsMasses  = []
+    matchedHiggsPts     = []
+    matchedYMasses      = []
+    matchedYPts         = []
+
+    for i,event in enumerate(tfile.Events):
+        # if(i>1000):
+        #     break
+
+        if(i%100000==0):
+            print(i)
+
+        higgsPhiEta         = getHigssPhiEta(event)
+        YPhiEta             = getYPhiEta(event)
+        b1PhiEta,b2PhiEta   = getBFromHiggsPhiEta(event)
+        b3PhiEta,b4PhiEta   = getBFromYPhiEta(event)
+        matchedHiggsJetIdxs = []
+        matchedYJetIdxs     = []
+        hDeltaRs            = []
+        YDeltaRs            = []
+        matchedHiggsFlag    = False
+        matchedYFlag        = False
+
+        for fatJetIdx in range(event.nFatJet):
+            fatJetPhiEta = event.FatJet_phi[fatJetIdx],event.FatJet_eta[fatJetIdx]
+
+            h_JetDR  = deltaR(fatJetPhiEta,higgsPhiEta)
+            b1_JetDR = deltaR(fatJetPhiEta,b1PhiEta)
+            b2_JetDR = deltaR(fatJetPhiEta,b2PhiEta)
+
+            Y_JetDR  = deltaR(fatJetPhiEta,YPhiEta)
+            b3_JetDR = deltaR(fatJetPhiEta,b3PhiEta)
+            b4_JetDR = deltaR(fatJetPhiEta,b4PhiEta)        
+
+            if (h_JetDR<0.8 and b1_JetDR<0.8 and b2_JetDR<0.8):
+                matchedHiggsJetIdxs.append(fatJetIdx)
+                hDeltaRs.append(h_JetDR)
+                matchedHiggsFlag=True
+                matchedHiggsMasses.append(event.FatJet_mass[fatJetIdx])
+                matchedHiggsPts.append(event.FatJet_pt[fatJetIdx])
+
+            if (Y_JetDR<0.8 and b3_JetDR<0.8 and b4_JetDR<0.8):
+                matchedYJetIdxs.append(fatJetIdx)
+                YDeltaRs.append(Y_JetDR)
+                matchedYFlag=True
+                matchedYMasses.append(event.FatJet_mass[fatJetIdx])
+                matchedYPts.append(event.FatJet_pt[fatJetIdx])
 
 
-    if(len(matchedYJetIdxs)>1):
-        print("Matched more than one AK8 to Y")
-        print("Y phi {0}, eta {1}".format(*YPhiEta))
-        for j,idx in enumerate(matchedYJetIdxs):
-            print("Jet{0} - pt {1}, mass {2}, phi {3}, eta {4}".format(j,event.FatJet_mass[idx],event.FatJet_pt[idx],event.FatJet_phi[idx],event.FatJet_eta[idx],))
-        print("DeltaRs (AK8-Y) {0},{1}".format(*YDeltaRs))
-        print('\n')
+        if(len(matchedHiggsJetIdxs)>1):
+            print("Matched more than one AK8 to Higgs")
+            print("Higgs phi {0}, eta {1}".format(*higgsPhiEta))
+            for j,idx in enumerate(matchedHiggsJetIdxs):
+                print("Jet{0} - pt {1}, mass {2}, phi {3}, eta {4}".format(j,event.FatJet_mass[idx],event.FatJet_pt[idx],event.FatJet_phi[idx],event.FatJet_eta[idx],))
+            print("DeltaRs (AK8-Higgs) {0},{1}".format(*hDeltaRs))
+            print('\n')
 
-    if matchedHiggsFlag:
-        matchedHiggs+=1
 
-    if matchedYFlag:
-        matchedY+=1
+        if(len(matchedYJetIdxs)>1):
+            print("Matched more than one AK8 to Y")
+            print("Y phi {0}, eta {1}".format(*YPhiEta))
+            for j,idx in enumerate(matchedYJetIdxs):
+                print("Jet{0} - pt {1}, mass {2}, phi {3}, eta {4}".format(j,event.FatJet_mass[idx],event.FatJet_pt[idx],event.FatJet_phi[idx],event.FatJet_eta[idx],))
+            print("DeltaRs (AK8-Y) {0},{1}".format(*YDeltaRs))
+            print('\n')
 
-    if matchedYFlag and matchedHiggsFlag:
-        matchedBoth+=1
+        if matchedHiggsFlag:
+            matchedHiggs+=1
 
-print(matchedHiggs)
-print(matchedY)
-print(matchedBoth)
+        if matchedYFlag:
+            matchedY+=1
 
-plotHistogram(matchedHiggsMasses,"FatJetMass_fromHiggs.png","AK8 from H mass histogram","Mass [GeV]")
-plotHistogram(matchedYMasses,"FatJetMass_fromY.png","AK8 from Y mass histogram","Mass [GeV]")
-plotHistogram(matchedHiggsPts,"FatJetPt_fromHiggs.png","AK8 from H pt histogram","Mass [GeV]")
-plotHistogram(matchedYPts,"FatJetPt_fromY.png","AK8 from Y pt histogram","Mass [GeV]")
+        if matchedYFlag and matchedHiggsFlag:
+            matchedBoth+=1
+
+    print(matchedHiggs)
+    print(matchedY)
+    print(matchedBoth)
+
+    plotHistogram(matchedHiggsMasses,outPrefix+"_FatJetMass_fromHiggs.png","AK8 from H mass histogram","Mass [GeV]",0.,600.)
+    plotHistogram(matchedYMasses,outPrefix+"_FatJetMass_fromY.png","AK8 from Y mass histogram","Mass [GeV]",0.,600.)
+    plotHistogram(matchedHiggsPts,outPrefix+"_FatJetPt_fromHiggs.png","AK8 from H pt histogram","Pt [GeV]",0.,2500.)
+    plotHistogram(matchedYPts,outPrefix+"_FatJetPt_fromY.png","AK8 from Y pt histogram","Pt [GeV]",0.,2500.)
+
+
+analyzeFile("E2FC3D3A-4D94-494D-BD56-524A28EF3C3F.root","mx2000")
