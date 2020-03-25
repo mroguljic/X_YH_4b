34357dd307f59c798392d277cfad056345240049
matej <matej.roguljic@gmail.com>
2020-03-25 14:29:45 +0100

    Fixed axes

--- DR_plotting.py
+++ DR_plotting.py
@@ -40,78 +40,57 @@ def singleCanvas(inputfile,outputFile,massPoints):
     r.gStyle.SetOptStat(0)
     c.Divide(2,2)
 
+    h1Dummy = r.TH2F("h1Dummy","DR matching efficiency for H",100,0.,2000.,100,0.,1.05)
+    h2Dummy = r.TH2F("h2Dummy","DR matching eff for Y",100,0.,2000.,100,0.,1.05)
+    h3Dummy = r.TH2F("h3Dummy","DR H-matched AK8 softdrop mass",100,0.,400.,100,0.,8400.)
+    h4Dummy = r.TH2F("h4Dummy","DR Y-matched AK8 softdrop mass",100,0.,500.,100,0.,9200.)
+    h1Dummy.SetXTitle("Pt [GeV]")
+    h2Dummy.SetXTitle("Pt [GeV]")
+    h1Dummy.SetYTitle("N_matched/N_total")
+    h2Dummy.SetYTitle("N_matched/N_total")
+
+    c.cd(1)
+    h1Dummy.Draw()
+    c.cd(2)
+    h2Dummy.Draw()
+    c.cd(3)
+    h3Dummy.Draw()
+    c.cd(4)
+    h4Dummy.Draw()
     for i,massPoint in enumerate(massPoints):
  
-        legend   = r.TLegend(0.1,0.7,0.48,0.9);
         colorIdx = i%nColors
         color    = colorCodes[colorIdx]
-        if(i==0):
-            h1      = inFile.Get("effMatchH_{0}".format(massPoint))#TEfficiency actually, not a TH1F
-            h2      = inFile.Get("effMatchY_{0}".format(massPoint))#TEfficiency actually, not a TH1F
-            h3      = inFile.Get("Hak8mass_{0}".format(massPoint))
-            h4      = inFile.Get("Yak8mass_{0}".format(massPoint))
 
-            h1.SetTitle("DR matching efficiency vs Pt for H")
-            h2.SetTitle("DR eff vs Pt for Y_{0}GeV".format(massPoint))
-            h3.SetTitle("Hak8mass_{0}".format(massPoint))
-            h4.SetTitle("Yak8mass_{0}".format(massPoint))
+        h1      = inFile.Get("effMatchH_{0}".format(massPoint))#TEfficiency actually, not a TH1F
+        h2      = inFile.Get("effMatchY_{0}".format(massPoint))#TEfficiency actually, not a TH1F
+        h3      = inFile.Get("Hak8mass_{0}".format(massPoint))
+        h4      = inFile.Get("Yak8mass_{0}".format(massPoint))
 
-            h1.SetLineColor(color) 
-            h2.SetLineColor(color) 
-            h3.SetLineColor(color) 
-            h4.SetLineColor(color)
-            #h1.SetFillColor(color) 
-            #h2.SetFillColor(color) 
-            h3.SetFillColor(color) 
-            h4.SetFillColor(color) 
+        h1.SetTitle("Heffiencies_{0}".format(massPoint))
+        h2.SetTitle("DR eff vs Pt for Y_{0}GeV".format(massPoint))
+        h3.SetTitle("Hak8mass_{0}".format(massPoint))
+        h4.SetTitle("Yak8mass_{0}".format(massPoint))
 
-            #h1.SetXTitle("Matched boson Pt [GeV]")
-            #h1.SetYTitle("AK8 matching efficiency")
-            #h2.SetXTitle("Matched boson Pt [GeV]")
-            #h2.SetYTitle("AK8 matching efficiency")
-            h3.SetXTitle("Higgs matched AK8 jet mass [GeV]")
-            h4.SetXTitle("Y matched AK8 jet mass [GeV]")
-
-            c.cd(1)
-            h1.Draw()
-            c.cd(2)
-            h2.Draw()
-            c.cd(3)
-            h3.Draw()
-            c.cd(4)
-            h4.Draw()
-            c.Modified()
-            c.Update()
-        else:
-            h1      = inFile.Get("effMatchH_{0}".format(massPoint))#TEfficiency actually, not a TH1F
-            h2      = inFile.Get("effMatchY_{0}".format(massPoint))#TEfficiency actually, not a TH1F
-            h3      = inFile.Get("Hak8mass_{0}".format(massPoint))
-            h4      = inFile.Get("Yak8mass_{0}".format(massPoint))
-
-            h1.SetTitle("Heffiencies_{0}".format(massPoint))
-            h2.SetTitle("DR eff vs Pt for Y_{0}GeV".format(massPoint))
-            h3.SetTitle("Hak8mass_{0}".format(massPoint))
-            h4.SetTitle("Yak8mass_{0}".format(massPoint))
-
-            h1.SetLineColor(color) 
-            h2.SetLineColor(color) 
-            h3.SetLineColor(color) 
-            h4.SetLineColor(color)
-            #h1.SetFillColor(color) 
-            #h2.SetFillColor(color) 
-            h3.SetFillColor(color) 
-            h4.SetFillColor(color) 
-           
-            c.cd(1)
-            #h1.Draw("SAME")
-            c.cd(2)
-            h2.Draw("SAME")
-            c.cd(3)
-            h3.Draw("SAME")
-            c.cd(4)
-            h4.Draw("SAME")
-            c.Modified()
-            c.Update()
+        h1.SetLineColor(color) 
+        h2.SetLineColor(color) 
+        h3.SetLineColor(color) 
+        h4.SetLineColor(color)
+        #h1.SetFillColor(color) 
+        #h2.SetFillColor(color) 
+        h3.SetFillColor(color) 
+        h4.SetFillColor(color) 
+       
+        c.cd(1)
+        h1.Draw("SAME")
+        c.cd(2)
+        h2.Draw("SAME")
+        c.cd(3)
+        h3.Draw("SAME")
+        c.cd(4)
+        h4.Draw("SAME")
+        c.Modified()
+        c.Update()
 
     c.cd(1).BuildLegend(0.6,0.1,0.9,0.4)
     c.cd(2).BuildLegend(0.6,0.1,0.9,0.4)
