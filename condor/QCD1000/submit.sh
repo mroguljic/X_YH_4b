905cafa968fa2745fab3cb37f7ef5b9c0a400a67
Matej Roguljic matej.roguljic@cern.ch <matej.roguljic@gmail.com>
2020-06-12 16:53:14 +0200

    Added condor configuration

---
+++ condor/QCD1000/submit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /afs/cern.ch/user/m/mrogulji/X_YH_4b/
+python eventSelection.py -i /afs/cern.ch/user/m/mrogulji/X_YH_4b/data/nanoTuples/QCD1000.txt -o /afs/cern.ch/user/m/mrogulji/X_YH_4b/condor/QCD1000/output.root -b -p QCD1000
+
---
+++ condor/QCD700/submit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /afs/cern.ch/user/m/mrogulji/X_YH_4b/
+python eventSelection.py -i /afs/cern.ch/user/m/mrogulji/X_YH_4b/data/nanoTuples/QCD700.txt -o /afs/cern.ch/user/m/mrogulji/X_YH_4b/condor/QCD700/output.root -b -p QCD700
+
