32aaf6a6dc7b1512aaf2e313a966ce9c5d49e21e
Matej Roguljic matej.roguljic@cern.ch <matej.roguljic@gmail.com>
2020-06-17 14:00:07 +0200

    Fixed legend and range

--- kinematicPlots.py
+++ kinematicPlots.py
@@ -5,12 +5,12 @@ import json
 
 
 def makeHistos(data):
-    luminosity = 137000 #inverse pb
-    hStack_TT = r.THStack("hstt","Stack histo TT")
-    hStack_LL = r.THStack("hsll","Stack histo LL")
-    legend = r.TLegend(0.6,0.7,0.9,0.9)
-    histos_TT = []
-    histos_LL = []
+    luminosity   = 137000 #inverse pb
+    hStack_TT    = r.THStack("hstt","Stack histo TT")
+    hStack_LL    = r.THStack("hsll","Stack histo LL")
+    legend       = r.TLegend(0.6,0.7,0.9,0.9)
+    histos_TT    = []
+    histos_LL    = []
     for sample, sample_cfg in data.items():
         tempFile = r.TFile.Open(sample_cfg['file'])
         hCutFlow = tempFile.Get("hCutFlow")
@@ -24,35 +24,45 @@ def makeHistos(data):
         LL_norm  = eff_LL*luminosity*xsec
         h_TT     = tempFile.Get((str(sample)+"_invMass_TT"))
         h_LL     = tempFile.Get((str(sample)+"_invMass_LL"))
+        color    = sample_cfg["color"]
         h_TT.SetDirectory(0)
         h_LL.SetDirectory(0)
         h_TT.Scale(TT_norm/h_TT.Integral())
         h_LL.Scale(LL_norm/h_LL.Integral())
         h_TT.SetTitle(str(sample))
         h_TT.SetName(str(sample)+"_name")
-        color   = sample_cfg["color"]
         h_TT.SetFillColorAlpha(color,0.50)
         h_LL.SetFillColorAlpha(color,0.50)
-        h_TT.SetLineColor(color)
-        h_LL.SetLineColor(color)
+        h_TT.SetLineWidth(0)
+        h_LL.SetLineWidth(0)
         histos_TT.append(h_TT)
         histos_LL.append(h_LL)
         tempFile.Close()
 
-    for h in histos_TT:
-        print(h)
-        hStack_TT.Add(h)
-        legend.AddEntry(h)
-    for h in histos_LL:
-        print(h)
-        hStack_LL.Add(h)
+    histos_TT.sort(key=lambda x: x.GetName())
+    histos_LL.sort(key=lambda x: x.GetName())
+    
+
     c = r.TCanvas("c","c",1000,1000)
     #c.SetLogy()
 
+    flagQCD = False
+    for h in histos_TT:
+        hStack_TT.Add(h)
+        if "QCD" in h.GetName():
+            if flagQCD==False:
+                legend.AddEntry(h,"QCD")
+                flagQCD=True
+            continue
+        legend.AddEntry(h)
+    for h in histos_LL:
+        hStack_LL.Add(h)
+
+
     hStack_TT.Draw("hist")
-    hStack_TT.GetXaxis().SetLimits(1300., 1800.);
+    hStack_TT.GetXaxis().SetLimits(1000., 3000.);
     hStack_TT.SetMinimum(1)
-    hStack_TT.SetMaximum(200)
+    hStack_TT.SetMaximum(600)
 
     legend.Draw()
     c.Update()
@@ -61,15 +71,22 @@ def makeHistos(data):
     c.Clear()
 
     hStack_LL.Draw("hist")
-    hStack_LL.GetXaxis().SetLimits(1300., 1800.);
+    hStack_LL.GetXaxis().SetLimits(1000., 3000.);
     hStack_LL.SetMinimum(1)
-    hStack_LL.SetMaximum(200)
+    hStack_LL.SetMaximum(2000)
     legend.Draw()
     c.Update()
     c.SaveAs("invM_LL.pdf")
 
 if __name__ == '__main__':
     r.gROOT.SetBatch()
-    with open("results.json") as json_file:
+    parser = OptionParser()
+    parser.add_option('-j', '--json', metavar='IFILE', type='string', action='store',
+                default   =   '',
+                dest      =   'json',
+                help      =   'Json file containing names, paths to histograms, xsecs etc.')
+    (options, args) = parser.parse_args()
+
+    with open(options.json) as json_file:
         data = json.load(json_file)
         makeHistos(data)
