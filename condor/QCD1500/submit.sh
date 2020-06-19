1ecfa4502f9b0b1c003d24008964d49ad9450a0b
Matej Roguljic matej.roguljic@cern.ch <matej.roguljic@gmail.com>
2020-06-19 09:57:55 +0200

    removing old files

--- condor/QCD1500/submit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /afs/cern.ch/user/m/mrogulji/X_YH_4b/
-python eventSelection.py -i /afs/cern.ch/user/m/mrogulji/X_YH_4b/data/nanoTuples/QCD1500.txt -o /afs/cern.ch/user/m/mrogulji/X_YH_4b/condor/QCD1500/output.root -b -p QCD1500
-
