e86ead171bd5f6125a8ca5c66b4668a26e7cf1d4
Matej Roguljic matej.roguljic@cern.ch <matej.roguljic@gmail.com>
2020-10-14 16:19:49 +0200

    small update

--- kinematicPlots.py
+++ kinematicPlots.py
@@ -3,23 +3,6 @@ from optparse import OptionParser
 from time import sleep
 import json
 
-def mergeQCDs(histos):
-    qcdHistFlag = False
-    idxToRemove = []
-    for i,h in enumerate(histos):
-        if not "QCD" in  h.GetTitle():
-            continue
-        if not qcdHistFlag:
-            hQCDIdx = i
-            qcdHistFlag = True
-            continue
-        histos[hQCDIdx].Add(h)
-        idxToRemove.append(i)
-
-    for idx in reversed(idxToRemove):
-        histos.pop(idx)
-    return histos
-
 def stackHistos(data,region,tagger,outFile):
     if(str(tagger) == "dak8"):
         taggerFull = "DeepAK8"
@@ -36,8 +19,6 @@ def stackHistos(data,region,tagger,outFile):
         histos.append(h)
 
     histos.sort(key=lambda x: x.GetName())
-    histos = mergeQCDs(histos)
-
     c = r.TCanvas("c","c",1500,1000)
     c.SetLogy()
     hSignal = False
@@ -61,7 +42,7 @@ def stackHistos(data,region,tagger,outFile):
     #hStack.SetMaximum(1300)
     hStack.SetMaximum(130000)
     if(hSignal):
-        legend.AddEntry(hSignal,"X: 1600 GeV","L")
+        legend.AddEntry(hSignal,"M_X, M_Y = 1600, 100 GeV","L")
         hSignal.Draw("hist same")
     
     hStack.GetXaxis().SetNdivisions(805, r.kTRUE);
@@ -78,7 +59,7 @@ def stackHistos(data,region,tagger,outFile):
     pt.SetLineWidth(0)
     pt.SetBorderSize(1)
     #pt.AddText("CMS Preliminary")
-    pt.AddText("#sigma #bf{( pp #rightarrow X #rightarrow HY #rightarrow b#bar{b}b#bar{b}) = 10 fb}")
+    pt.AddText("#sigma #bf{( pp #rightarrow X #rightarrow HY #rightarrow b#bar{b}b#bar{b}) = 1 pb}")
     pt.Draw()
 
     pt2 = r.TPaveText(0.71,0.92,0.9,0.95,"NDC")
@@ -96,15 +77,10 @@ def stackHistos(data,region,tagger,outFile):
 
 def getInvMass_h (sample,sample_cfg,region,tagger,luminosity=137000):#inverse pb
     tempFile = r.TFile.Open(sample_cfg["file"])
-    hCutFlow = tempFile.Get("hCutFlow_{0}".format(tagger))
-    h2d      = tempFile.Get("{0}_mjHY_mjH_{1}_{2}".format(sample,tagger,region))
-    nTotal   = hCutFlow.GetBinContent(1)
-    nRegion  = h2d.Integral()
-    xsec     = sample_cfg['xsec']    
-    eff      = nRegion/nTotal
-    norm     = eff*luminosity*xsec
-    hInvMass = h2d.ProjectionX("{0}_mjHY_{1}_{2}".format(sample,tagger,region))
-    hInvMass.Scale(norm/hInvMass.Integral())
+    print(tempFile)
+    print("{0}_mjH_mjHY_{1}_{2}".format(sample,tagger,region))
+    h2d      = tempFile.Get("{0}_mjH_mjjHY_{1}_{2}".format(sample,tagger,region))
+    hInvMass = h2d.ProjectionY("{0}_mjjHY_{1}_{2}".format(sample,tagger,region))
     hInvMass.SetTitle("{0}_{1}_{2} HY invariant mass".format(sample,tagger,region))   
     hInvMass.Rebin(10) 
     color    = sample_cfg["color"]
@@ -199,8 +175,6 @@ def nMinusOnePlot(data,cut,outFile,xTitle="",yTitle="",rangeX=[],stackTitle="",s
     c.SaveAs(outFile)
 
 
-
-
 if __name__ == '__main__':
     r.gROOT.SetBatch()
     parser = OptionParser()
@@ -212,17 +186,17 @@ if __name__ == '__main__':
 
     with open(options.json) as json_file:
         data = json.load(json_file)
-        cuts = ["DeltaEta","ptjH","ptjY","mjH","mjY","pnetH","pnetY"]
-        nMinusOnePlot(data,"mjY","mjY.png",rangeX=[0,500],xTitle="mSD Y-jet [GeV]",yTitle="Events / 10 GeV",stackTitle="N-1: mSD for Y-tagged jets",sigLabel="M_X, M_Y = 1600, 100 GeV")
-        nMinusOnePlot(data,"mjH","mjH.png",rangeX=[0,500],xTitle="mSD H-jet [GeV]",yTitle="Events / 10 GeV",stackTitle="N-1: mSD for H-tagged jets",sigLabel="M_X, M_Y = 1600, 100 GeV")
-        nMinusOnePlot(data,"ptjH","ptjH.png",rangeX=[0,2000],xTitle="pT H-jet [GeV]",yTitle="Events / 20 GeV",stackTitle="N-1: pT for H-tagged jets",sigLabel="M_X, M_Y = 1600, 100 GeV")
-        nMinusOnePlot(data,"ptjY","ptjY.png",rangeX=[0,2000],xTitle="pT Y-jet [GeV]",yTitle="Events / 20 GeV",stackTitle="N-1: pT for Y-tagged jets",sigLabel="M_X, M_Y = 1600, 100 GeV")
-        nMinusOnePlot(data,"DeltaEta","DeltaEta.png",rangeX=[0,5],xTitle="mSD [GeV]",yTitle="Events/bin",stackTitle="N-1: |eta_{1} - eta_{2}|",sigLabel="M_X, M_Y = 1600, 100 GeV")
-        nMinusOnePlot(data,"pnetH","pnetH.png",rangeX=[0,1],xTitle="ParticleNetMD_probXbb score",yTitle="Events/bin",stackTitle="N-1: b-tagger for H-tagged jets",sigLabel="M_X, M_Y = 1600, 100 GeV")
-        nMinusOnePlot(data,"pnetY","pnetY.png",rangeX=[0,1],xTitle="ParticleNetMD_probXbb score",yTitle="Events/bin",stackTitle="N-1: b-tagger for Y-tagged jets",sigLabel="M_X, M_Y = 1600, 100 GeV")
-        # for cut in cuts:
+        #cuts = ["DeltaEta","ptjH","ptjY","mjH","mjY","pnetH","pnetY"]
+        # nMinusOnePlot(data,"mjY","mjY.png",rangeX=[0,500],xTitle="mSD Y-jet [GeV]",yTitle="Events / 10 GeV",stackTitle="N-1: mSD for Y-tagged jets",sigLabel="M_X, M_Y = 1600, 100 GeV")
+        # nMinusOnePlot(data,"mjH","mjH.png",rangeX=[0,500],xTitle="mSD H-jet [GeV]",yTitle="Events / 10 GeV",stackTitle="N-1: mSD for H-tagged jets",sigLabel="M_X, M_Y = 1600, 100 GeV")
+        # nMinusOnePlot(data,"ptjH","ptjH.png",rangeX=[0,2000],xTitle="pT H-jet [GeV]",yTitle="Events / 20 GeV",stackTitle="N-1: pT for H-tagged jets",sigLabel="M_X, M_Y = 1600, 100 GeV")
+        # nMinusOnePlot(data,"ptjY","ptjY.png",rangeX=[0,2000],xTitle="pT Y-jet [GeV]",yTitle="Events / 20 GeV",stackTitle="N-1: pT for Y-tagged jets",sigLabel="M_X, M_Y = 1600, 100 GeV")
+        # nMinusOnePlot(data,"DeltaEta","DeltaEta.png",rangeX=[0,5],xTitle="mSD [GeV]",yTitle="Events/bin",stackTitle="N-1: |eta_{1} - eta_{2}|",sigLabel="M_X, M_Y = 1600, 100 GeV")
+        # nMinusOnePlot(data,"pnetH","pnetH.png",rangeX=[0,1],xTitle="ParticleNetMD_probXbb score",yTitle="Events/bin",stackTitle="N-1: b-tagger for H-tagged jets",sigLabel="M_X, M_Y = 1600, 100 GeV")
+        # nMinusOnePlot(data,"pnetY","pnetY.png",rangeX=[0,1],xTitle="ParticleNetMD_probXbb score",yTitle="Events/bin",stackTitle="N-1: b-tagger for Y-tagged jets",sigLabel="M_X, M_Y = 1600, 100 GeV")
+        # # for cut in cuts:
         #     nMinusOnePlot(data,cut,"{0}.png".format(cut))
-        # stackHistos(data,"TT","pnet","TT_pnet.pdf")
-        # stackHistos(data,"LL","pnet","LL_pnet.pdf")
-        # stackHistos(data,"TT","dak8","TT_dak8.pdf")
-        # stackHistos(data,"LL","dak8","LL_dak8.pdf")
+        stackHistos(data,"TT","pnet","TT_pnet.pdf")
+        stackHistos(data,"LL","pnet","LL_pnet.pdf")
+        stackHistos(data,"TT","dak8","TT_dak8.pdf")
+        stackHistos(data,"LL","dak8","LL_dak8.pdf")
