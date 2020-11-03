6713fa3bc89cc37d68ec02fd53a0089f0e2586af
Matej Roguljic matej.roguljic@cern.ch <matej.roguljic@gmail.com>
2020-11-03 17:58:47 +0100

    added function to create pseudo from QCD and ttbar

--- normalizeSamples.py
+++ normalizeSamples.py
@@ -10,7 +10,7 @@ def normalizeProcess(process,xsec,luminosity,outFile):
     file     = "/afs/cern.ch/user/m/mrogulji/X_YH_4b/results/histograms/nonScaled/{0}.root".format(process)
     f        = r.TFile.Open(file)
     print(file)
-    hCutFlow = f.Get("hCutFlow")
+    hCutFlow = f.Get("{0}_cutflow".format(process))
     nProc    = hCutFlow.GetBinContent(1)#number of processed events
     nLumi    = xsec*luminosity
     scaling  = nLumi/nProc
@@ -44,7 +44,7 @@ def mergeHT(fJson,luminosity,process,outFile):
             file     = "/afs/cern.ch/user/m/mrogulji/X_YH_4b/results/histograms/nonScaled/{0}.root".format(sample)
             f        = r.TFile.Open(file)
             print(file)
-            hCutFlow = f.Get("hCutFlow")
+            hCutFlow = hCutFlow = f.Get("{0}_cutflow".format(sample))
             nProc    = hCutFlow.GetBinContent(1)#number of processed events
             nLumi    = xsec*luminosity
             scaling  = nLumi/nProc
@@ -56,10 +56,7 @@ def mergeHT(fJson,luminosity,process,outFile):
                 #     h.Scale(scaling)
                 h.Scale(scaling)
                 h.SetDirectory(0)
-                if ("hCutFlow" in hName):
-                    hKey = hName
-                else:
-                    hKey = re.sub("{0}\d+_".format(process),"{0}_".format(process),hName)
+                hKey = re.sub("{0}\d+_".format(process),"{0}_".format(process),hName)
                 if not hKey in h_dict:
                     h.SetName(hKey)
                     h_dict[hKey] = h
@@ -73,42 +70,14 @@ def mergeHT(fJson,luminosity,process,outFile):
         histo.Write()
     f.Close()
 
-def mergeData(inFiles,samples,outFile):
-    for inFile in inFiles:
-        f        = r.TFile.Open(inFile) 
-        h_dict = {}   
-        for key in f.GetListOfKeys():
-            h = key.ReadObj()
-            hName = h.GetName()
-            h.SetDirectory(0)
-            if ("hCutFlow" in hName):
-                hKey = hName
-            else:
-                hKey = re.sub("Data.","data_obs",hName)
-            if not hKey in h_dict:
-                h.SetName(hKey)
-                h_dict[hKey] = h
-            else:
-                h_dict[hKey].Add(h)
-        f.Close()
-    f = r.TFile(outFile,"recreate")
-    f.cd()
-    for key in h_dict:
-        histo = h_dict[key]
-        histo.Write()
-    f.Close()
-
-def createPseudo(inFile,outFile):
+def mergeData(inFile,outFile):
     f        = r.TFile.Open(inFile) 
     h_dict = {}   
     for key in f.GetListOfKeys():
         h = key.ReadObj()
         hName = h.GetName()
         h.SetDirectory(0)
-        if ("hCutFlow" in hName):
-            hKey = hName
-        else:
-            hKey = re.sub(".+_","data_obs_",hName)
+        hKey = re.sub("Data.","data_obs",hName)
         if not hKey in h_dict:
             h.SetName(hKey)
             h_dict[hKey] = h
@@ -122,22 +91,81 @@ def createPseudo(inFile,outFile):
         histo.Write()
     f.Close()
 
+def mergeSamples(inFiles,outFile,regexMatch,regexReplace):
+    for inFile in inFiles:
+        print(inFile)
+        f        = r.TFile.Open(inFile) 
+        h_dict = {}   
+        for key in f.GetListOfKeys():
+            h = key.ReadObj()
+            hName = h.GetName()
+            h.SetDirectory(0)
+            hKey = re.sub(regexMatch,regexReplace,hName,count=1)
+            if not hKey in h_dict:
+                h.SetName(hKey)
+                h_dict[hKey] = h
+            else:
+                h_dict[hKey].Add(h)
+        f.Close()
+    f = r.TFile(outFile,"recreate")
+    f.cd()
+    for key in h_dict:
+        histo = h_dict[key]
+        histo.Write()
+    f.Close()
+
+def createPseudo(inFiles,outFile):
+    for inFile in inFiles:
+        f      = r.TFile.Open(inFile) 
+        h_dict = {}   
+        for key in f.GetListOfKeys():
+            h = key.ReadObj()
+            hName = h.GetName()
+            h.SetDirectory(0)
+            hKey = re.sub("[a-zA-Z]+","data_obs",hName,count=1)
+            if not hKey in h_dict:
+                h.SetName(hKey)
+                h_dict[hKey] = h
+            else:
+                h_dict[hKey].Add(h)
+        f.Close()
+    f = r.TFile(outFile,"recreate")
+    f.cd()
+    for key in h_dict:
+        histo = h_dict[key]
+        histo.Write()
+    f.Close()
+
 
 
 if __name__ == '__main__':
-    luminosity = 137000
+    luminosity = 35900
     with open(sys.argv[1]) as json_file:
         config = json.load(json_file)
         for sample, sample_cfg in config.items():
-            print(sample)
             if("QCD" in sample):
                 continue
             if("WJets" in sample):
                 continue
             if("ZJets" in sample):
                 continue
-            #normalizeProcess(sample,sample_cfg['xsec'],luminosity,"results/histograms/lumiScaled/{0}_normalized.root".format(sample))
-    # mergeHT(sys.argv[1],luminosity,"QCD","results/histograms/lumiScaled/QCD_normalized.root")    
+    #         normalizeProcess(sample,sample_cfg['xsec'],luminosity,"results/histograms/lumiScaled/{0}_normalized.root".format(sample))
+    # mergeHT(sys.argv[1],luminosity,"QCD","results/histograms/lumiScaled/QCD_normalized.root")
     # mergeHT(sys.argv[1],luminosity,"ZJets","results/histograms/lumiScaled/ZJets_normalized.root")
     # mergeHT(sys.argv[1],luminosity,"WJets","results/histograms/lumiScaled/WJets_normalized.root")
-    mergeData("/afs/cern.ch/user/m/mrogulji/X_YH_4b/results/histograms/nonScaled/data_obs.root","/afs/cern.ch/user/m/mrogulji/X_YH_4b/results/histograms/lumiScaled/data_obs.root")
+
+    #mergeData("/afs/cern.ch/user/m/mrogulji/X_YH_4b/results/histograms/nonScaled/data_obs.root","/afs/cern.ch/user/m/mrogulji/X_YH_4b/results/histograms/lumiScaled/data_obs.root")
+    createPseudo(["results/histograms/lumiScaled/QCD_normalized.root","results/histograms/lumiScaled/ttbar_normalized.root"],"/afs/cern.ch/user/m/mrogulji/X_YH_4b/results/histograms/lumiScaled/pseudodata.root")
+    
+#     ST_files = ["results/histograms/lumiScaled/ST_antitop_normalized.root",
+# "results/histograms/lumiScaled/ST_top_normalized.root",
+# "results/histograms/lumiScaled/ST_tW_antitop_normalized.root",
+# "results/histograms/lumiScaled/ST_tW_top_normalized.root"]
+#     VH_files = ["results/histograms/lumiScaled/WminusH_normalized.root",
+# "results/histograms/lumiScaled/WplusH_normalized.root",
+# "results/histograms/lumiScaled/ZH_normalized.root"]
+#     VJets_files = ["results/histograms/lumiScaled/ZJets_normalized.root",
+# "results/histograms/lumiScaled/WJets_normalized.root"]
+#     mergeSamples(ST_files,"results/histograms/lumiScaled/ST_normalized.root",".+top_","ST_")    
+#     mergeSamples(VH_files,"results/histograms/lumiScaled/VH_normalized.root","[a-zA-Z]+H","VH")
+#     mergeSamples(VJets_files,"results/histograms/lumiScaled/VJets_normalized.root","[A-Z]Jets","VJets")
