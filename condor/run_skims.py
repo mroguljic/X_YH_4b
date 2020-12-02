d211842d302c0aab0511092f02f39fdd75bd21f5
Matej Roguljic matej.roguljic@cern.ch <matej.roguljic@gmail.com>
2020-12-02 14:44:43 +0100

    added number of files per job option

--- condor/run_skims.py
+++ condor/run_skims.py
@@ -12,7 +12,7 @@ def split_jobs(files, njobs):
 
 
 
-def create_jobs(config, dry=True, queue='',year="2016",jobs_dir="",out_dir=""):
+def create_jobs(config, dry=True, queue='',year="2016",jobs_dir="",out_dir="",nFiles=1):
     for sample, sample_cfg in config.items():
       #print(sample)
       if re.match('^X', sample):
@@ -55,8 +55,8 @@ def create_jobs(config, dry=True, queue='',year="2016",jobs_dir="",out_dir=""):
         files = subprocess.check_output(das_query, shell=True).split()
         for file in files:
           allFiles.append(file.decode("utf-8"))
-      njobs = int(max(1, len(allFiles)/300))
-      job_list = split_jobs(allFiles, njobs)
+      job_list = split_jobs(allFiles, nFiles)
+
 
       #Create file with arguments to the python script
       argsFile = open(os.path.join(sampleJobs_dir, 'input', 'args_{}.txt'.format(sample)), 'w')
@@ -86,15 +86,17 @@ def main():
   parser.add_argument('-d', '--dry', action='store_true', help='Dry run (Do not submit jobs)')
   parser.add_argument('-o', '--outdir',help='Output directory')
   parser.add_argument('-j', '--jobdir',help='Jobs directory')
+  parser.add_argument('-n', '--nFiles',help='Number of files per job',type=int,default=1)
   parser.add_argument("-q", "--queue", dest="queue", action='store', default='longlunch', help="Default is 'longlunch' (This parameter is optional)", metavar="QUEUE")
  
   args = parser.parse_args()
 
   print(args)
-
+#/eos/cms/store/group/phys_b2g/mrogulji/skims/2016
+#/afs/cern.ch/work/m/mrogulji/X_YH_4b/condor/skimJobs/2016
   with open(args.config, 'r') as config_file:
     config = json.load(config_file)
-    create_jobs(config, args.dry,queue=args.queue, year=args.year,out_dir=args.outdir,jobs_dir=args.jobdir)
+    create_jobs(config, args.dry,queue=args.queue, year=args.year,out_dir=args.outdir,jobs_dir=args.jobdir,nFiles=args.nFiles)
           
 
       
