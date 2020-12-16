5a8791306cc3180b6ebb91a886b481558e59ed17
mrogulji <matej.roguljic@cern.ch>
2020-12-16 11:44:59 +0100

    added semileptonic submit script

--- condor/run_semiLeptonicSelection.py
+++ condor/run_semiLeptonicSelection.py
@@ -11,15 +11,8 @@ def split_jobs(files, njobs):
 
 
 
-def create_jobs(config, dry=True, queue='',year="2016",jobs_dir="",out_dir="",nFiles=1):
-    for sample, sample_cfg in config.items():
-      if re.match('^X', sample):
-        proctype = 'sig'
-      elif re.match('^Data', sample):
-        proctype = 'data'
-      else:
-        proctype = 'bkg'
-      
+def create_jobs(config, dry=True, queue='',year="2016",jobs_dir="",out_dir="",nFiles=1,leptonChannel="mu"):
+    for sample, sample_cfg in config.items():      
       sampleJobs_dir = os.path.join(jobs_dir,sample)
       sampleOut_dir = os.path.join(out_dir, sample)
       #Create dir to store jobs and dir to store output
@@ -28,10 +21,10 @@ def create_jobs(config, dry=True, queue='',year="2016",jobs_dir="",out_dir="",nF
       Path(sampleOut_dir).mkdir(parents=True, exist_ok=True)
 
       #Create condor file and sh file
-      exeScript = selection_template.replace("JOB_DIR",sampleJobs_dir)
+      exeScript = semileptonic_template.replace("JOB_DIR",sampleJobs_dir)
       open(os.path.join(sampleJobs_dir, 'input', 'run_{}.sh'.format(sample)), 'w').write(exeScript)
 
-      condor_script = re.sub('EXEC',os.path.join(sampleJobs_dir, 'input', 'run_{}.sh'.format(sample)), selection_condor)
+      condor_script = re.sub('EXEC',os.path.join(sampleJobs_dir, 'input', 'run_{}.sh'.format(sample)), semileptonic_condor)
       condor_script = re.sub('ARGFILE',os.path.join(sampleJobs_dir, 'input', 'args_{}.txt'.format(sample)), condor_script)
       condor_script = re.sub('OUTPUT',os.path.join(sampleJobs_dir, 'output'), condor_script)
       condor_script = re.sub('QUEUE',queue, condor_script)
@@ -45,15 +38,11 @@ def create_jobs(config, dry=True, queue='',year="2016",jobs_dir="",out_dir="",nF
 
       #Create file with arguments to the python script
       argsFile = open(os.path.join(sampleJobs_dir, 'input', 'args_{}.txt'.format(sample)), 'w')
-      if(proctype=="sig"):
-        ymass = "-m {0}".format(sample_cfg["YMass"])
-      else:
-        ymass = ''
       for n, l  in enumerate(list(job_list)):
         inputPath = os.path.join(sampleJobs_dir, 'input', 'input_{}.txt'.format(n))
         outputPath = os.path.join(sampleOut_dir,'{0}_{1}.root'.format(sample,n))
         open(inputPath, 'w').writelines("{}\n".format(root_file) for root_file in l)
-        argsFile.write("-i {0} -o {1} --{2} -p {3} {4} -y {5}\n".format(inputPath,outputPath,proctype,sample,ymass,year))
+        argsFile.write("-i {0} -o {1} -p {2} -y {3} -c {4}\n".format(inputPath,outputPath,sample,year,leptonChannel))
       #Submit
       print("condor_submit {0}".format(os.path.join(sampleJobs_dir, 'input', 'condor_{}.condor'.format(sample))))
       if dry==False:
@@ -67,12 +56,12 @@ def main():
   from argparse import ArgumentParser
   parser = ArgumentParser(description="Do -h to see usage")
 
-  parser.add_argument('-v', '--verbose', action='store_true')
   parser.add_argument('-c', '--config', help='Job config file in JSON format')
   parser.add_argument('-y', '--year', help='Dataset year',default="2016")
   parser.add_argument('-d', '--dry', action='store_true', help='Dry run (Do not submit jobs)')
   parser.add_argument('-o', '--outdir',help='Output directory')
   parser.add_argument('-j', '--jobdir',help='Jobs directory')
+  parser.add_argument('-l', '--lepton',help='Lepton channel (mu/e)')
   parser.add_argument('-n', '--nFiles',help='Number of files per job',type=int,default=1)
   parser.add_argument("-q", "--queue", dest="queue", action='store', default='longlunch', help="Default is 'longlunch' (This parameter is optional)", metavar="QUEUE")
  
@@ -82,7 +71,7 @@ def main():
 
   with open(args.config, 'r') as config_file:
     config = json.load(config_file)
-    create_jobs(config, args.dry,queue=args.queue, year=args.year,out_dir=args.outdir,jobs_dir=args.jobdir,nFiles=args.nFiles)
+    create_jobs(config, args.dry,queue=args.queue, year=args.year,out_dir=args.outdir,jobs_dir=args.jobdir,nFiles=args.nFiles,leptonChannel=args.lepton)
           
 
       
