46780fe5921e4a32734b63598903ea6edca2f3d1
Devdatta <devdatta.majumder@cern.ch>
2020-12-22 13:58:06 +0100

    changing paths

--- condor/templates.py
+++ condor/templates.py
@@ -1,6 +1,6 @@
 run_script_template='''#!/bin/bash
 
-cd /afs/cern.ch/work/m/mrogulji/X_YH_4b/CMSSW_11_1_5/src
+cd /afs/cern.ch/user/d/devdatta/afswork/CMSREL/Analysis/X_YH_4b/CMSSW_11_1_5/
 eval `scramv1 runtime -sh`
 cd /afs/cern.ch/work/m/mrogulji/X_YH_4b
 source timber-env/bin/activate
@@ -40,7 +40,7 @@ queue
 
 selection_template='''#!/bin/bash
 
-cd /afs/cern.ch/work/m/mrogulji/X_YH_4b/CMSSW_11_1_5/src
+cd /afs/cern.ch/user/d/devdatta/afswork/CMSREL/Analysis/X_YH_4b/CMSSW_11_1_5/
 eval `scramv1 runtime -sh`
 cd /afs/cern.ch/work/m/mrogulji/X_YH_4b
 source timber-env/bin/activate
@@ -53,10 +53,10 @@ python ${WORK_DIR}/eventSelection.py $*
 '''
 
 skim_template='''#!/bin/bash
-export WORK_DIR=/afs/cern.ch/work/m/mrogulji/X_YH_4b/
-cd /afs/cern.ch/work/m/mrogulji/X_YH_4b/CMSSW_11_1_5/src
+export WORK_DIR=/afs/cern.ch/user/d/devdatta/afswork/CMSREL/Analysis/X_YH_4b/X_YH_4b/
+cd /afs/cern.ch/user/d/devdatta/afswork/CMSREL/Analysis/X_YH_4b/CMSSW_11_1_5/
 eval `scramv1 runtime -sh`
-cd /afs/cern.ch/work/m/mrogulji/X_YH_4b
+cd /afs/cern.ch/user/d/devdatta/afswork/CMSREL/Analysis/X_YH_4b/
 source timber-env/bin/activate
 cd JOB_DIR
 echo ${WORK_DIR}/skim.py $*
