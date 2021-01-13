e85932af32f08d819d87526313689b207f216a37
mrogulji <matej.roguljic@cern.ch>
2021-01-13 08:03:22 +0100

    hadd evt selection output

--- haddScript.py
+++ haddScript.py
@@ -1,7 +1,9 @@
 import os
 
 directories=[d for d in os.listdir(os.getcwd()) if os.path.isdir(d)]
+variations = ["nom","jesUp","jesDown","jerUp","jerDown","jmsUp","jmsDown","jmrUp","jmrDown"]
 for d in directories:
-    cmd = "hadd -f {0}.root {0}/*root".format(d)
-    print(cmd)
-    os.system(cmd)
+    for variation in variations:
+        cmd = "hadd -f {0}_{1}.root {0}/*{1}*root".format(d,variation)
+        print(cmd)
+        os.system(cmd)
