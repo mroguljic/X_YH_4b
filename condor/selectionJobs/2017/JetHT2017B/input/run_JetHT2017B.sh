22d19cde9dc6302112aba34c5f8904830a4189d6
mrogulji <matej.roguljic@cern.ch>
2021-03-09 16:23:38 +0100

    example of a condor job

---
+++ condor/selectionJobs/2017/JetHT2017B/input/run_JetHT2017B.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /afs/cern.ch/work/m/mrogulji/UL_X_YH/CMSSW_11_1_5/
+eval `scramv1 runtime -sh`
+cd /afs/cern.ch/work/m/mrogulji/UL_X_YH/
+source timber-env/bin/activate
+
+export WORK_DIR=/afs/cern.ch/work/m/mrogulji/UL_X_YH/X_YH_4b//
+cd /afs/cern.ch/work/m/mrogulji/UL_X_YH/X_YH_4b/condor/selectionJobs/2017/JetHT2017B
+
+echo eventSelection.py $*
+python $WORK_DIR/eventSelection.py $* --var nom
