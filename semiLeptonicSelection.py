8b1dedc84faafa7f69daed5093b1284a09061ce5
mrogulji <matej.roguljic@cern.ch>
2021-03-23 08:58:57 +0100

    update for UL

--- semiLeptonicSelection.py
+++ semiLeptonicSelection.py
@@ -12,24 +12,12 @@ from TIMBER.Analyzer import *
 TIMBERPATH = os.environ["TIMBERPATH"]
 
 
-def getNProc(inputFile):
-    nProc        = 0
-    if ".root" in inputFile:
-        tempFile = ROOT.TFile.Open(inputFile)
-        skimInfo = tempFile.Get("skimInfo")
-        nProc    = skimInfo.GetBinContent(1)
-        tempFile.Close()
-        return nProc
-    elif ".txt" in inputFile: 
-        txt_file     = open(inputFile,"r")
-        for l in txt_file.readlines():
-            thisfile = l.strip()
-            if 'root://' not in thisfile and thisfile.startswith('/store/'): thisfile='root://cms-xrd-global.cern.ch/'+thisfile
-            tempFile = ROOT.TFile.Open(thisfile)
-            skimInfo = tempFile.Get("skimInfo")
-            nProc    += skimInfo.GetBinContent(1)
-            tempFile.Close()
-    return nProc
+def getNweighted(analyzer,isData):
+    if not isData:
+        nWeighted = a.DataFrame.Sum("genWeight").GetValue()
+    else:
+        nWeighted = a.DataFrame.Count().GetValue()
+    return nWeighted
 
 parser = OptionParser()
 
@@ -45,10 +33,6 @@ parser.add_option('-p', '--process', metavar='PROCESS', type='string', action='s
                 default   =   'ttbarSemi',
                 dest      =   'process',
                 help      =   'Process in the given MC file')
-parser.add_option('-c', '--channel', metavar='CHANNEL', type='string', action='store',
-                default   =   'mu',
-                dest      =   'channel',
-                help      =   'Electron (e) or muon (mu) channel')
 parser.add_option('-y', '--year', metavar='year', type='string', action='store',
                 default   =   '2016',
                 dest      =   'year',
@@ -57,6 +41,10 @@ parser.add_option('--var', metavar='variation', type='string', action='store',
                 default   =   "nom",
                 dest      =   'variation',
                 help      =   'nom sfUp/Down, jesUp/Down, jerUp/Down')
+# parser.add_option('-c', '--channel', metavar='CHANNEL', type='string', action='store', #only running mu channel for now
+#                 default   =   'mu',
+#                 dest      =   'channel',
+#                 help      =   'Electron (e) or muon (mu) channel')
 
 (options, args) = parser.parse_args()
 start_time = time.time()
@@ -83,12 +71,15 @@ if(isData and variation!="nom"):
     print("Not running systematics on data")
     sys.exit()
 
+if isData:
+    a.Define("genWeight","1")
 
-nProc = 0
+nProc = a.genEventSumw
+
 if not isData:
     CompileCpp('TIMBER/Framework/src/AK4Btag_SF.cc')
+    print('AK4Btag_SF ak4SF = AK4Btag_SF({0}, "DeepJet", "reshaping");'.format(options.year[2:]))
     CompileCpp('AK4Btag_SF ak4SF = AK4Btag_SF({0}, "DeepJet", "reshaping");'.format(options.year[2:]))
-    nProc = getNProc(options.input)
 
 if("je" in variation):#jes,jer
     ptVar  = "Jet_pt_{0}".format(variation.replace("jes","jesTotal"))
@@ -103,49 +94,42 @@ if(isData):
 
 histos      = []
 
-deepJetM = 0.3093
-deepJetL = 0.0614
+if(options.year=="2016"):
+    deepJetM = 0.3093
+    deepJetL = 0.0614
 if(options.year=="2017"):
-    deepJetM = 0.3033
-    deepJetL = 0.0521 
+    deepJetM = 0.3040 
+    deepJetL = 0.0532 
 if(options.year=="2018"):
-    deepJetM = 0.2770
-    deepJetL = 0.0494 
+    deepJetM = 0.2783 
+    deepJetL = 0.0490 
 
 histos=[]
-if(options.channel=="mu"):
-    if(options.year=="2016"):
-        triggerList = ["HLT_IsoMu24","HLT_IsoTkMu24"]
-    elif(options.year=="2017"):
-        triggerList = ["HLT_IsoMu27"]
-    else:
-        triggerList = ["HLT_IsoMu24"]
-    lGeneration = 2
-    a.Cut("leptonSkimCut","SkimFlag>7")
-
-elif(options.channel=="e"):
-    triggerList = ["HLT_Ele27_WPTight_Gsf","HLT_Ele35_WPTight_Gsf","HLT_Ele32_WPTight_Gsf","HLT_Photon200","HLT_Photon175"]
-    lGeneration = 1
-    a.Cut("leptonSkimCut","SkimFlag>3 && SkimFlag<8")
+if(options.year=="2016"):
+    triggerList = ["HLT_IsoMu24","HLT_IsoTkMu24"]
+elif(options.year=="2017"):
+    triggerList = ["HLT_IsoMu27"]
 else:
-    print("Define lepton channel '-c mu' or '-c e'!")
-    sys.exit()
+    triggerList = ["HLT_IsoMu24"]
+lGeneration = 2
 
-if not isData:
-    nSkimmed = a.DataFrame.Sum("genWeight").GetValue()
-else:
-    nSkimmed = a.DataFrame.Count().GetValue()
+#if(options.channel=="mu"):
+# elif(options.channel=="e"):
+#     triggerList = ["HLT_Ele27_WPTight_Gsf","HLT_Ele35_WPTight_Gsf","HLT_Ele32_WPTight_Gsf","HLT_Photon200","HLT_Photon175"]
+#     lGeneration = 1
+#     a.Cut("leptonSkimCut","SkimFlag>3 && SkimFlag<8")
+# else:
+#     print("Define lepton channel '-c mu' or '-c e'!")
+#     sys.exit()
 
+nSkimmed = getNweighted(a,isData)
 
 
 if(isData):
     triggerString = a.GetTriggerString(triggerList)
     a.Cut("Triggers",triggerString)  
 
-if not isData:    
-    nTrigger = a.DataFrame.Sum("genWeight").GetValue() 
-else:
-    nTrigger = a.DataFrame.Count().GetValue()
+nTrigger = getNweighted(a,isData)
 
 if(variation == "sfDown"):
     sfVar = 1
@@ -162,28 +146,19 @@ else:
 a.Define("lIdx","tightLeptonIdx(nElectron,Electron_cutBased,nMuon,Muon_tightId,Muon_pfIsoId,{0})".format(lGeneration))
 a.Cut("lIdxCut","lIdx>-1")#There is a tight lepton
 
-if not isData:    
-    nTightLepton = a.DataFrame.Sum("genWeight").GetValue() 
-else:
-    nTightLepton = a.DataFrame.Count().GetValue()
+nTightLepton = getNweighted(a,isData)
 
 a.Define("lPt","leptonPt(Electron_pt,Muon_pt,lIdx,{0})".format(lGeneration))
 a.Define("lPhi","leptonPhi(Electron_phi,Muon_phi,lIdx,{0})".format(lGeneration))
 a.Define("lEta","leptonEta(Electron_eta,Muon_eta,lIdx,{0})".format(lGeneration))
 a.Cut("lPtCut","lPt>40")
 
-if not isData:    
-    nlPt = a.DataFrame.Sum("genWeight").GetValue() 
-else:
-    nlPt = a.DataFrame.Count().GetValue()
+nlPt = getNweighted(a,isData)
 
 a.Cut("lEtaCut","abs(lEta)<2.4")
 a.Cut("METcut","{0}>60".format(METVar))
 
-if not isData:    
-    nMET = a.DataFrame.Sum("genWeight").GetValue() 
-else:
-    nMET = a.DataFrame.Count().GetValue()
+nMET = getNweighted(a,isData)
 
 
 a.Define("goodJetIdxs","goodAK4JetIdxs(nJet, {0}, Jet_eta, Jet_phi, Jet_jetId, lPhi, lEta)".format(ptVar))#pt>30,|eta|<2.4, dR(lepton,jet)>0.4
@@ -193,53 +168,41 @@ a.Define("leptonSideAK4Idx","leptonSideJetIdx(goodJetIdxs,Jet_eta,Jet_phi,btagDi
 a.Cut("leptonSideAK4Cut","leptonSideAK4Idx>-1")
 a.Define("bJetPt","{0}[leptonSideAK4Idx]".format(ptVar))
 
-if not isData:    
-    nBJetLeptonSide = a.DataFrame.Sum("genWeight").GetValue() 
-else:
-    nBJetLeptonSide = a.DataFrame.Count().GetValue()
+nBJetLeptonSide = getNweighted(a,isData)
 
 
 a.Define("HT","HTgoodJets({0}, goodJetIdxs)".format(ptVar))
 a.Cut("HTCut","HT>500")
 
-if not isData:    
-    nHT = a.DataFrame.Sum("genWeight").GetValue() 
-else:
-    nHT = a.DataFrame.Count().GetValue()
-
+nHT = getNweighted(a,isData)
 
 a.Define("probeJetIdx","probeAK8JetIdx(nFatJet,Fat{0},FatJet_msoftdrop,FatJet_phi,FatJet_eta,FatJet_jetId,lPhi,lEta)".format(ptVar))
 a.Cut("probeJetIdxCut","probeJetIdx>-1")
-if not isData:    
-    a.Define("probeJetPt",'FatJet_pt[probeJetIdx]')
-    nProbeJet = a.DataFrame.Sum("genWeight").GetValue() 
-else:
-    a.Define("probeJetPt","Fat{0}[probeJetIdx]".format(ptVar))
-    nProbeJet = a.DataFrame.Count().GetValue()
+a.Define("probeJetPt","Fat{0}[probeJetIdx]".format(ptVar))
+if(options.year=="2017" or options.year=="2018"):
+    a.Cut("probeJetPtCut","probeJetPt>450")#350 is used in probeJetIdx
 
+nProbeJet = getNweighted(a,isData)
 
 a.Define("ST","bJetPt+{0}+lPt+probeJetPt".format(METVar))
 a.Cut("STcut","ST>500")
 
-if not isData:    
-    nST = a.DataFrame.Sum("genWeight").GetValue() 
-else:
-    nST = a.DataFrame.Count().GetValue()
+nST = getNweighted(a,isData)
 
 
 #Define branches to store in output
 if(isData):
     a.Define("probeJetMass_nom",'FatJet_msoftdrop[probeJetIdx]')
-    a.Define("probeJetPNet","FatJet_ParticleNetMD_probXbb[probeJetIdx]")
+    a.Define("probeJetPNet","FatJet_particleNetMD_Xbb[probeJetIdx]/(FatJet_particleNetMD_Xbb[probeJetIdx]+FatJet_particleNetMD_QCD[probeJetIdx])")
 else:
     a.Define("scaledProbeJetNom",'jmsShifter.shiftMsd(FatJet_msoftdrop[probeJetIdx],"{0}",0)'.format(options.year))
     a.Define("scaledProbeJetUp",'jmsShifter.shiftMsd(FatJet_msoftdrop[probeJetIdx],"{0}",2)'.format(options.year))
     a.Define("scaledProbeJetDown",'jmsShifter.shiftMsd(FatJet_msoftdrop[probeJetIdx],"{0}",1)'.format(options.year))
-    smearStringNom = "scaledProbeJetNom,1.0,GenJetAK8_mass,FatJet_genJetAK8Idx[probeJetIdx],1"
-    smearStringJMSUp = "scaledProbeJetUp,1.0,GenJetAK8_mass,FatJet_genJetAK8Idx[probeJetIdx],1"
-    smearStringJMSDown = "scaledProbeJetDown,1.0,GenJetAK8_mass,FatJet_genJetAK8Idx[probeJetIdx],1"
-    smearStringJMRUp = "scaledProbeJetNom,1.1,GenJetAK8_mass,FatJet_genJetAK8Idx[probeJetIdx],2"
-    smearStringJMRDown = "scaledProbeJetNom,1.1,GenJetAK8_mass,FatJet_genJetAK8Idx[probeJetIdx],0"
+    smearStringNom = "scaledProbeJetNom,1.0,nGenJetAK8,GenJetAK8_mass,FatJet_genJetAK8Idx[probeJetIdx],1"
+    smearStringJMSUp = "scaledProbeJetUp,1.0,nGenJetAK8,GenJetAK8_mass,FatJet_genJetAK8Idx[probeJetIdx],1"
+    smearStringJMSDown = "scaledProbeJetDown,1.0,nGenJetAK8,GenJetAK8_mass,FatJet_genJetAK8Idx[probeJetIdx],1"
+    smearStringJMRUp = "scaledProbeJetNom,1.1,nGenJetAK8,GenJetAK8_mass,FatJet_genJetAK8Idx[probeJetIdx],2"
+    smearStringJMRDown = "scaledProbeJetNom,1.1,nGenJetAK8,GenJetAK8_mass,FatJet_genJetAK8Idx[probeJetIdx],0"
 
     a.Define("probeJetMass_nom",'jmrSmearer.smearMsd({0})'.format((smearStringNom)))
     a.Define("probeJetMass_jmsUp",'jmrSmearer.smearMsd({0})'.format((smearStringJMSUp)))
@@ -247,7 +210,7 @@ else:
     a.Define("probeJetMass_jmrDown",'jmrSmearer.smearMsd({0})'.format((smearStringJMRDown)))
     a.Define("probeJetMass_jmrUp",'jmrSmearer.smearMsd({0})'.format((smearStringJMRUp)))
 
-    a.Define("probeJetPNet","FatJet_ParticleNetMD_probXbb[probeJetIdx]")
+    a.Define("probeJetPNet","FatJet_particleNetMD_Xbb[probeJetIdx]/(FatJet_particleNetMD_Xbb[probeJetIdx]+FatJet_particleNetMD_QCD[probeJetIdx])")
     a.Define("partonCategory","classifyProbeJet(probeJetIdx, FatJet_phi, FatJet_eta, nGenPart, GenPart_phi, GenPart_eta, GenPart_pdgId, GenPart_genPartIdxMother)")
 
 
