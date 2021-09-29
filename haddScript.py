8549e2bb5b021c785431fdf04e31e8156fb083cc
mrogulji <matej.roguljic@cern.ch>
2021-09-29 16:15:37 +0200

    small update

--- haddScript.py
+++ haddScript.py
@@ -1,9 +1,27 @@
 import os
 
 directories=[d for d in os.listdir(os.getcwd()) if os.path.isdir(d)]
-variations = ["nom","jesUp","jesDown","jerUp","jerDown","jmsUp","jmsDown","jmrUp","jmrDown"]
+if("semi" in os.getcwd()):
+    semil      = True
+    variations = ["nom","sfUp","sfDown","jesUp","jesDown","jerUp","jerDown"] #CR
+else:
+    semil      = False
+    variations = ["nom","jesUp","jesDown","jerUp","jerDown","jmsUp","jmsDown","jmrUp","jmrDown","MJYrotUp","MJYrotDown"]
 for d in directories:
+    if("MX" in d):
+        continue
     for variation in variations:
-        cmd = "hadd -f {0}_{1}.root {0}/*{1}*root".format(d,variation)
+        if("MJYrot" in variation and not "TTbar" in d):
+            continue
+        if(semil):
+            if(variation!="nom" and "Single" in d):
+                continue
+        else:
+            if(variation!="nom" and not ("MX" in d or "TTbar" in d)):
+                continue
+        if("MX" in d):
+            cmd = "mv {0}/{0}_0_{1}.root {0}_{1}.root".format(d,variation)
+        else:
+            cmd = "hadd -f {0}_{1}.root {0}/*{1}*root".format(d,variation)
         print(cmd)
         os.system(cmd)
