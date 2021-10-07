0ee234531eb47b025b07978d0b3614014027f726
mrogulji <matej.roguljic@cern.ch>
2021-10-07 14:43:36 +0200

    semileptonic cutflow

--- cutFlowSummary.py
+++ cutFlowSummary.py
@@ -11,76 +11,132 @@ parser.add_option('-j', '--json', metavar='IFILE', type='string', action='store'
             default   =   '',
             dest      =   'json',
             help      =   'Json file containing names, paths to histograms, xsecs etc.')
+parser.add_option('-c', '--channel', metavar='IFILE', type='string', action='store',
+            default   =   'hadronic',
+            dest      =   'channel',
+            help      =   'Hadronic or semileptonic')
 
 (options, args) = parser.parse_args()
 
-totalBkg = []
 
-additionalRegions = ["L_AL","T_AL"]
+if(options.channel=="hadronic"):
+    totalBkg = []
 
-with open(options.json) as json_file:
-    labels = ["Sample"]
-    values = []
-    data = json.load(json_file)
-    labelFlag = False
+    additionalRegions = ["L_AL","T_AL"]
 
-    for sample, sample_cfg in data.items():
-        if ("MX" in sample):
-            datasetType = "sig"
-        elif ("JetHT" in sample or "data" in sample or "SingleMuon" in sample):
-            datasetType = "data"
-        else:
-            datasetType = "bkg"
+    with open(options.json) as json_file:
+        labels = ["Sample"]
+        values = []
+        data = json.load(json_file)
+        labelFlag = False
+
+        for sample, sample_cfg in data.items():
+            if ("MX" in sample):
+                datasetType = "sig"
+            elif ("JetHT" in sample or "data" in sample or "SingleMuon" in sample):
+                datasetType = "data"
+            else:
+                datasetType = "bkg"
+                
+
+            f = r.TFile.Open(sample_cfg["file"])
+            h = f.Get("{0}_cutflow_nom".format(sample))
+            nBin = h.GetNbinsX()
+            totalBkgRow = []
+            latexRow = sample+" &"
+            #for i in range(1,nBin+1):
+            for i in range(1,nBin-3):
+                if(labelFlag==False):
+                    labels.append(h.GetXaxis().GetBinLabel(i).replace(" ","_"))
+                nCut = h.GetBinContent(i)
+                #nCut = nCut/(h.GetBinContent(1))
+                if(nCut>10000):
+                    latexRow = latexRow +" {0:1.2e} &".format(nCut)
+                else:   
+                    latexRow = latexRow +" {0:.3f} &".format(nCut)
+                totalBkgRow.append(nCut)
+
+            if(labelFlag==False):
+                labelFlag=True
+                print(" & ".join(labels), " \\\\")
+
+            #Additional regions
+            for region in additionalRegions:
+                nCut = f.Get("{0}_mJY_mJJ_{1}_nom".format(sample,region)).Integral()
+                #nCut = nCut/(h.GetBinContent(1))
+                if(nCut>10000):
+                    latexRow = latexRow +" {0:1.2e} &".format(nCut)
+                else:   
+                    latexRow = latexRow +" {0:.3f} &".format(nCut)
+                totalBkgRow.append(nCut)
+            #-----#
             
 
-        f = r.TFile.Open(sample_cfg["file"])
-        h = f.Get("{0}_cutflow_nom".format(sample))
-        nBin = h.GetNbinsX()
-        totalBkgRow = []
-        latexRow = sample+" &"
-        #for i in range(1,nBin+1):
-        for i in range(1,nBin-3):
-            if(labelFlag==False):
-                labels.append(h.GetXaxis().GetBinLabel(i).replace(" ","_"))
-            nCut = h.GetBinContent(i)
-            nCut = nCut/(h.GetBinContent(1))
-            if(nCut>10000):
-                latexRow = latexRow +" {0:1.2e} &".format(nCut)
-            else:   
-                latexRow = latexRow +" {0:.3f} &".format(nCut)
-            totalBkgRow.append(nCut)
+            latexRow = latexRow[:-2]+" \\\\"
+            print(latexRow)
+            if(datasetType=="bkg"):
+                if len(totalBkg)==0:
+                    totalBkg=totalBkgRow
+                else:
+                    totalBkg = np.add(totalBkg,totalBkgRow)
+            # a = latexRow.split(" & ")
+            # print("{0} {1} {2}".format(a[0],a[-4],a[-5]))
 
-        if(labelFlag==False):
-            labelFlag=True
-            print(" & ".join(labels), " \\\\")
+    latexTotalBkg = "Total_bkg "
+    for val in totalBkg:
+        if(val>10000):
+            latexTotalBkg += "& {0:1.2e} ".format(val)
+        else:
+            latexTotalBkg += "& {0:.2f} ".format(val)
+    print(latexTotalBkg+"\\\\")
 
-        #Additional regions
-        for region in additionalRegions:
-            nCut = f.Get("{0}_mJY_mJJ_{1}_nom".format(sample,region)).Integral()
-            nCut = nCut/(h.GetBinContent(1))
-            if(nCut>10000):
-                latexRow = latexRow +" {0:1.2e} &".format(nCut)
-            else:   
-                latexRow = latexRow +" {0:.3f} &".format(nCut)
-            totalBkgRow.append(nCut)
-        #-----#
-        
+else:
+    with open(options.json) as json_file:
+        labels    = ["Sample"]
+        values    = []
+        data      = json.load(json_file)
+        labelFlag = False
 
-        latexRow = latexRow[:-2]+" \\\\"
-        print(latexRow)
-        if(datasetType=="bkg"):
-            if len(totalBkg)==0:
-                totalBkg=totalBkgRow
+
+        for sample, sample_cfg in data.items():
+            f = r.TFile.Open(sample_cfg["file"])
+            if("data_obs" in sample):
+                h = f.Get("{0}_cutflow".format(sample))
             else:
-                totalBkg = np.add(totalBkg,totalBkgRow)
-        # a = latexRow.split(" & ")
-        # print("{0} {1} {2}".format(a[0],a[-4],a[-5]))
+                h = f.Get("{0}_cutflow_nom".format(sample))
+            nBin = h.GetNbinsX()
+            totalBkgRow = []
+            latexRow = sample+" &"
+            for i in range(1,nBin):
+                if(labelFlag==False):
+                    labels.append(h.GetXaxis().GetBinLabel(i).replace(" ","_"))
+                nCut = h.GetBinContent(i)
+                #nCut = nCut/(h.GetBinContent(1))
+                if(nCut>10000):
+                    latexRow = latexRow +" {0:1.2e} &".format(nCut)
+                else:   
+                    latexRow = latexRow +" {0:.3f} &".format(nCut)
+                totalBkgRow.append(nCut)
 
-latexTotalBkg = "Total_bkg "
-for val in totalBkg:
-    if(val>10000):
-        latexTotalBkg += "& {0:1.2e} ".format(val)
-    else:
-        latexTotalBkg += "& {0:.2f} ".format(val)
-print(latexTotalBkg+"\\\\")
+            if(labelFlag==False):
+                labelFlag=True
+                print(" & ".join(labels), " \\\\")
+            
+            #Additional regions
+            for region in ["L","T"]:
+                if("data_obs" in sample):
+                    nCut = f.Get("{0}_mSD_{1}".format(sample,region)).Integral()
+                else:
+                    nCut = f.Get("{0}_mSD_{1}_nom".format(sample,region)).Integral()
+                #nCut = nCut/(h.GetBinContent(1))
+                if(nCut>10000):
+                    latexRow = latexRow +" {0:1.2e} &".format(nCut)
+                else:   
+                    latexRow = latexRow +" {0:.3f} &".format(nCut)
+                totalBkgRow.append(nCut)
+            #-----#
+
+
+            latexRow = latexRow[:-2]+" \\\\"
+            print(latexRow)
 
